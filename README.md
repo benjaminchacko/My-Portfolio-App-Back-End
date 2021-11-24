5a5a1de6c1a938130515d5276da655a671aa271b
Benjamin Chacko <benjaminchacko@outlook.com>
2021-11-23 18:46:36 -0700

    refactoring

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
-# My Portfolio App - Back End
+# My School Portfolio App - Back End
 [![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)](https://www.github.com/benjaminchacko/My-Portfolio-App-Back-End/graphs/commit-activity)
+![Last Updated:](https://img.shields.io/badge/Last%20Updated-11%7C23%20%7C2021-yellow.svg)
+![Version](https://img.shields.io/badge/Version-1.0-green.svg)
+![License](https://img.shields.io/badge/License-MIT-blue.svg)
 
-This repository is the Express JS - Back End of My Portfolio App. Click [here](https://github.com/benjaminchacko/My-Portfolio-App-Front-End) to explore the React JS - Front End.
+This repository is the Express.js - backend part of my portfolio app I built in 2019 after graduating from Bethel School of Technology with a Certificate in Web Development. In November 2021, I updated the project with some of my recent knowledge, including refactoring the project structure and further documentation of the project.
 
-I used Express JS to showcase the back end web developer skills learned at Bethel School of Technlogy, powered by Woz-U.
+Click [here](https://github.com/benjaminchacko/My-Portfolio-App-Front-End) to explore the React JS - Front End.
+
 
 ### :scroll: License 
 This repository is published under the terms of the [MIT license](https://github.com/benjaminchacko/React-Counter-App/blob/master/LICENSE).
-© [Benjamin T. Chacko](https://GitHub.com/benjaminchacko), 2019
+© [Benjamin T. Chacko](https://GitHub.com/benjaminchacko), 2021
 
 [![forthebadge](https://forthebadge.com/images/badges/built-by-developers.svg)](https://forthebadge.com)
 [![forthebadge](https://forthebadge.com/images/badges/made-with-javascript.svg)](https://forthebadge.com)
 [![forthebadge](https://forthebadge.com/images/badges/uses-git.svg)](https://forthebadge.com)
+
+## How to Use
+1. Fork this repository
+2. Clone to your local machine
+3. Create a .env file in root directory like this example: 
+<img src="./assets/env-example.PNG" />
+
+4. `yarn install` in your terminal to install node-modules
+5. `yarn dev` to start Nodemon dev server
+6. Use Postman, Insomnia or a similar tool to test API endpoints
--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-// const session = require('express-session');
-// const MongoStore = require('connect-mongo')(session);
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('We are on home');
+// Middleware 
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+  res.locals.session = req.session;
+  next();
+});
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
 });
 
 // Enviroment Variables
@@ -22,22 +35,26 @@ mongoose.connect(process.env.DB_CONNECT, {
   .then(() => console.log('connection successful'))
   .catch((err) => console.error(err));
 mongoose.Promise = global.Promise;
-
-const blogRoutes = require('./api/routes/blogs');
-const fireBaseRoute = require('./api/routes/fileUploader');
-const loginRoutes = require('./api/routes/login');
-const signupRoutes = require('./api/routes/signup');
   
 // Routes that should handle requests
-app.use('/blogs', blogRoutes);
-app.use('/login', loginRoutes);
-app.use('/signup', signupRoutes);
-app.use('/upload', fireBaseRoute);
+app.use('/blogs', require('./api/routes/blogs'));
+app.use('/login', require('./api/routes/login'));
+app.use('/signup', require('./api/routes/signup'));
+app.use('/upload', require('./api/routes/fileUploader'));
 
-/* app.use(session({
-  secret: 'honeyBadger',
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection
-  })
-})); */
-module.exports = app;
+// MongoDB Connection 
+mongoose.connect(process.env.DB_CONNECTION, {
+  promiseLibrary: require('bluebird'),
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('connection to MongoDB server: success'))
+  .catch((err) => console.error(err));
+mongoose.Promise = global.Promise;
+
+const port = process.env.port || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
+server.on('error', console.error);
